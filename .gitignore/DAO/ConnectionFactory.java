f6219743bf7c577366305ff5edb0253ca2d6fae3
Guilherme Fogo <gui.fogo17@gmail.com>
2018-07-15 18:01:28 -0300

    Add files via upload

---
+++ .gitignore/DAO/ConnectionFactory.java
@@ -0,0 +1,22 @@
+package DAO;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+
+import com.mysql.jdbc.Driver;
+
+public class ConnectionFactory {
+	private Connection conexao;
+	public Connection conecta_mysql() {
+		try {
+			Class.forName("com.mysql.jdbc.Driver");
+			DriverManager.registerDriver(new Driver());
+			conexao = DriverManager.getConnection("jdbc:mysql://localhost/teste_db","root", "123456");
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		return conexao;
+		
+	}
+
+}
